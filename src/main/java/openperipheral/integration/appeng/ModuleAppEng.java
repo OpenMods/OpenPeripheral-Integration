35452f08c0c4de828cd0c1483c122851e639eb78
Bartek Bok <bartek.bok@gmail.com>
2014-11-10 17:27:49 +0100

    AppEng - nope. Needs overhaul

--- src/main/java/openperipheral/integration/appeng/ModuleAppEng.java
+++ src/main/java/openperipheral/integration/appeng/ModuleAppEng.java
@@ -1,9 +1,6 @@
 package openperipheral.integration.appeng;
 
 import openmods.Mods;
-import openperipheral.api.ApiAccess;
-import openperipheral.api.IAdapterRegistry;
-import openperipheral.api.ITypeConvertersRegistry;
 import openperipheral.integration.OPIntegrationModule;
 
 public class ModuleAppEng extends OPIntegrationModule {
@@ -15,12 +12,6 @@ public class ModuleAppEng extends OPIntegrationModule {
 
 	@Override
 	public void load() {
-		IAdapterRegistry adapterRegistry = ApiAccess.getApi(IAdapterRegistry.class);
-		adapterRegistry.register(new AdapterCellProvider());
-		adapterRegistry.register(new AdapterGridTileEntity());
-		adapterRegistry.register(new AdapterTileController());
-
-		ITypeConvertersRegistry typeRegistry = ApiAccess.getApi(ITypeConvertersRegistry.class);
-		typeRegistry.register(new ConverterIItemList());
+		// TODO reeimplement from base (new API)
 	}
 }
