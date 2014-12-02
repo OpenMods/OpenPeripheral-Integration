71c433ec0fd2a7abe919fc2015f156b59b155d9e
Bartek Bok <bartek.bok@gmail.com>
2014-12-02 08:26:25 +0100

    Fix CoFH transport

--- src/main/java/openperipheral/integration/cofh/transport/ModuleCofhTransport.java
+++ src/main/java/openperipheral/integration/cofh/transport/ModuleCofhTransport.java
@@ -2,11 +2,7 @@ package openperipheral.integration.cofh.transport;
 
 import openperipheral.api.ApiAccess;
 import openperipheral.api.IAdapterRegistry;
-import openperipheral.api.IItemStackMetaBuilder;
 import openperipheral.integration.ApiIntegrationModule;
-import openperipheral.integration.cofh.energy.AdapterEnergyReceiver;
-import openperipheral.integration.cofh.energy.EnergyMetaProvider;
-import openperipheral.integration.cofh.tileentity.AdapterEnergyInfo;
 
 public class ModuleCofhTransport extends ApiIntegrationModule {
 
@@ -18,11 +14,8 @@ public class ModuleCofhTransport extends ApiIntegrationModule {
 	@Override
 	public void load() {
 		final IAdapterRegistry adapterRegistry = ApiAccess.getApi(IAdapterRegistry.class);
-		adapterRegistry.register(new AdapterEnergyReceiver());
+		adapterRegistry.register(new AdapterEnderEnergyAttuned());
 		adapterRegistry.register(new AdapterEnderItemAttuned());
-		adapterRegistry.register(new AdapterEnergyInfo());
-
-		final IItemStackMetaBuilder itemMeta = ApiAccess.getApi(IItemStackMetaBuilder.class);
-		itemMeta.register(new EnergyMetaProvider());
+		adapterRegistry.register(new AdapterEnderFluidAttuned());
 	}
 }
