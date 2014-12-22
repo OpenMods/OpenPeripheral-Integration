2d985a461270f45773d75b72ea21c2f0b3cfdfd7
Bartek Bok <bartek.bok@gmail.com>
2014-12-22 19:31:38 +0100

    Remove few methods due to sideness

--- src/main/java/openperipheral/integration/minefactoryreloaded/AdapterAutoJukebox.java
+++ src/main/java/openperipheral/integration/minefactoryreloaded/AdapterAutoJukebox.java
@@ -1,19 +1,18 @@
 package openperipheral.integration.minefactoryreloaded;
 
-import openmods.reflection.*;
+import openmods.reflection.MethodAccess;
 import openmods.reflection.MethodAccess.Function0;
-import openmods.reflection.MethodAccess.Function1;
-import openperipheral.api.*;
+import openmods.reflection.ReflectionHelper;
+import openperipheral.api.IPeripheralAdapter;
+import openperipheral.api.LuaCallable;
+import openperipheral.api.LuaReturnType;
 
 public class AdapterAutoJukebox implements IPeripheralAdapter {
 
 	private final Class<?> CLASS = ReflectionHelper.getClass("powercrystals.minefactoryreloaded.tile.machine.TileEntityAutoJukebox");
 
 	private final Function0<Boolean> GET_CAN_COPY = MethodAccess.create(boolean.class, CLASS, "getCanCopy");
-	private final Function1<Void, Boolean> SET_CAN_COPY = MethodAccess.create(void.class, CLASS, boolean.class, "setCanCopy");
-
 	private final Function0<Boolean> GET_CAN_PLAY = MethodAccess.create(boolean.class, CLASS, "getCanPlay");
-	private final Function1<Void, Boolean> SET_CAN_PLAY = MethodAccess.create(void.class, CLASS, boolean.class, "setCanPlay");
 
 	private final Function0<Void> COPY = MethodAccess.create(void.class, CLASS, "copyRecord");
 	private final Function0<Void> PLAY = MethodAccess.create(void.class, CLASS, "playRecord");
@@ -34,19 +33,9 @@ public class AdapterAutoJukebox implements IPeripheralAdapter {
 		return GET_CAN_COPY.call(tileEntityAutoJukebox);
 	}
 
-	@LuaCallable(description = "Set wheather a disc can be copied")
-	public void setCanCopy(Object tileEntityAutoJukebox, @Arg(name = "copyable") boolean copyable) {
-		SET_CAN_COPY.call(tileEntityAutoJukebox, copyable);
-	}
-
 	@LuaCallable(description = "Can a disc be played?", returnTypes = LuaReturnType.BOOLEAN)
 	public boolean getCanPlay(Object tileEntityAutoJukebox) {
 		return GET_CAN_PLAY.call(tileEntityAutoJukebox);
-	}
-
-	@LuaCallable(description = "Set wheather a disc can be played")
-	public void setCanPlay(Object tileEntityAutoJukebox, @Arg(name = "playable") boolean playable) {
-		SET_CAN_PLAY.call(tileEntityAutoJukebox, playable);
 	}
 
 	@LuaCallable(description = "Copy record")
